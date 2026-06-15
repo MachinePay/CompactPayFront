1799ced84691f5df5590e1b78a9bee0eba3cd442
Gabriel <gakirakiyohara@gmail.com>
2026-06-15 16:31:58 -0300

    arrumando mobile p

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,9 @@ import Sidebar from "./Sidebar";
 
 export default function Layout({ children }) {
   return (
-    <div className="app-shell flex min-h-screen flex-col gap-3 p-2 pt-[68px] sm:p-3 sm:pt-[72px] lg:flex-row lg:gap-4 lg:p-5">
+    <div className="app-shell flex min-h-screen flex-col gap-3 px-3 pb-3 pt-[76px] sm:p-3 sm:pt-[72px] lg:flex-row lg:gap-4 lg:p-5">
       <Sidebar />
-      <main className="min-w-0 flex-1 overflow-visible rounded-[18px] bg-white/36 p-1 sm:p-2 lg:overflow-hidden lg:rounded-[28px]">
+      <main className="min-w-0 flex-1 overflow-visible rounded-[18px] bg-white/36 p-2 sm:p-2 lg:overflow-hidden lg:rounded-[28px]">
         {children}
       </main>
     </div>
